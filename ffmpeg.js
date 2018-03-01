d4c4dc1a29756f2c150717994a9273510949b071
Samfox2 <Samfox2@users.noreply.github.com>
2018-03-01 19:57:06 +0100

    Update ffmpeg.js
    Bugfix

--- ffmpeg.js
+++ ffmpeg.js
@@ -334,6 +334,12 @@ FFMPEG.prototype.createCameraControlService = function() {
   var controlService = new Service.CameraControl();
 
   this.services.push(controlService);
+  
+  if(this.audio){
+    var microphoneService = new Service.Microphone();
+    this.services.push(microphoneService);
+  }
+  
 }
 
 // Private
